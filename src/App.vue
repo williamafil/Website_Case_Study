b86aa8504f921e2a8a14f30b08d2b079195256ec
ToroChu <whileonrails@gmail.com>
2021-07-15 17:56:04 +0800

    add sass and fix router path and components path

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
+  <Header />
+  <router-view />
+
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header.vue'
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+
+// export default {
+//   name: 'App',
+//   components: {
+//     Header,
+//     HelloWorld
+//   }
+// }
 </script>
 
 <style>
 #app {
+  margin-top: 60px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  text-align: center;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import("../views/Home.vue"),
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("../views/About.vue"),
+  },
+];
+
+export default createRouter({
+  history: createWebHistory(),
+  routes,
+});
