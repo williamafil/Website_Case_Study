9a1eee0343152b6f1b6e596f632df5bf6b4f9050
William <will2b@gmail.com>
2021-08-09 20:20:46 +0800

    package.json

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/Website_Case_Study/',
-  // resolve: {
-  //   alias: {
-  //     '@': path.resolve(__dirname, './src'), // 设置别名
-  //   },
-  // },
 });
