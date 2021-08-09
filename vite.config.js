cc80e2955c55965426644a7ba702aaf28e1f6cbb
William <will2b@gmail.com>
2021-08-09 20:31:21 +0800

    change base url

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Website_Case_Study/',
+  base: '/',
+  // 别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // 设置别名
+    },
+  },
 });
